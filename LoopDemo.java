ecc504ae3db68ef9f2732340c80514cbe975dce9
ganesh peddisetti <168344926+ganeshpeddisetti31@users.noreply.github.com>
2025-12-24 10:07:28 +0530

    day-3

---
+++ LoopDemo.java
@@ -0,0 +1,10 @@
+ public class LoopDemo {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i = i + 1) {
+            System.out.println(i);
+        }
+
+    }
+}
+
